19f4e20bea671450416253dbb9c51ff986c1c820
Wanli_Lu <41014384+IDGAQ@users.noreply.github.com>
2021-03-10 10:47:30 +0800

    Create notes.java

---
+++ notes.java
@@ -0,0 +1,9 @@
+// string to char
+for(i...) charAt(i);
+
+//char to int
+int a;
+char b ='5';
+a = b-'0';
+//// because the value is read as ASCII code
+//// in ASCII, numbers starts at 48, and '0' is exactly 48!
