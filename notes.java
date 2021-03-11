92a4a15ef6751c25c2a0f7667f8d708e62f90c60
Wanli_Lu <41014384+IDGAQ@users.noreply.github.com>
2021-03-12 01:45:41 +0800

    Update notes.java

--- notes.java
+++ notes.java
@@ -1,9 +1,26 @@
 // string to char
 for(i...) charAt(i);
 
+
 //char to int
+//// because the value is read as ASCII code
+//// in ASCII, numbers starts at 48, and '0' is exactly 48!
 int a;
 char b ='5';
 a = b-'0';
-//// because the value is read as ASCII code
-//// in ASCII, numbers starts at 48, and '0' is exactly 48!
+
+
+//use of ^
+(boolean)
+if( true ^ true ) return false;
+if (false ^ true) return true;
+if (true ^ false) return false;
+if(false ^ false) return true;
+(int)
+int a=3;
+int b=4;
+// 3 = 0011   4 = 0100
+// a = 0011^0100 = 0111 a=7
+a^=b;
+// b = 4 ^ 7, do the same
+b^=a;
